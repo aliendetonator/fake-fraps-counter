177ebdd2829f20effe83fd3d1d3108b7e266af97
Balázs lászlóm <alien.detonator@gmail.com>
2023-08-31 04:30:26 +0200

    init

---
+++ .history/src/script_20230831033010.js
@@ -0,0 +1,43 @@
+const counter = document.getElementById("counter");
+
+const fps = {
+    default: 60,
+    min: 57,
+    max: 60,
+    chanceOfDefault: 80
+}
+const interval = {
+    min: 100,
+    max: 300
+}
+
+var fpsTimeout;
+
+const getNextIntervalTime = () => {
+    return Math.floor(Math.random() * (interval.max - interval.min) ) + interval.min;
+}
+
+const getNextFps = () => {
+
+    let chancePercentage = Math.floor(Math.random()*100) + 1;
+
+    if (chancePercentage <= fps.chanceOfDefault) return fps.default;
+
+    return Math.floor(Math.random() * (fps.max - fps.min) ) + fps.min;
+}
+
+const setNextInterval = () => {
+    fpsTimeout = setTimeout(()=>{
+
+        counter.innerText = getNextFps();
+
+        setNextInterval();
+    }
+    ,getNextIntervalTime())
+}
+
+const main = () => {
+    setNextInterval();
+}
+
+main();
